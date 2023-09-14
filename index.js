1635b58d08dabba6c42082417978387521ba4624
JMADA257 <jadams1812@hotmail.com>
2023-09-14 15:28:06 -0600

    Updated code so produces a readme file

--- index.js
+++ index.js
@@ -7,64 +7,103 @@ inquirer
   .prompt([
     {
       type: "input",
-      name: "project-name",
+      name: "projectTitle",
       message: "Hello, Please enter your project name!",
     },
     {
-        type: "input",
-        name: "description",
-        message: "Please give me a description about your project!",
-      },
-      {
-        type: "input",
-        name: "table-of-contents",
-        message: "If your READ.me is large enough please give me some items I can arrange into a table of contents!", //will need to arrange this into a list, possibly with buttons linking them to where they are on the readme
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "What installations or tools required to are used in this project?",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "What are the uses of this project?", // maybe put these into a list?
-      },
-      {
-        type: "input",
-        name: "credits",
-        message: "Please list other collaborators that helped you in this project with links to there github profile! Also any third-party assets that require attribution. Finally any tutorials please include the link to them as well.", //put these in a list 
-      },
-      {
-        type: "input",
-        name: "badges",
-        message: "Please present any badges as well.",
-      },
-      {
-        type: "input",
-        name: "features",
-        message: "Now list the features this project has!", //another list for this?
-      },
-      {
-        type: "input",
-        name: "contribution",
-        message: "How can a user contribute to this project?",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "Finally, how can we test this application to make sure everything runs smoothly?",
-      },
-
-
-
+      type: "input",
+      name: "description",
+      message: "Please give me a description about your project!",
+    },
+    {
+      type: "input",
+      name: "tableOfContents",
+      message:
+        "If your READ.me is large enough please give me some items I can arrange into a table of contents!", //will need to arrange this into a list, possibly with buttons linking them to where they are on the readme
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "What installations or tools required to are used in this project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What are the uses of this project?", // maybe put these into a list?
+    },
+    {
+      type: "input",
+      name: "credits",
+      message:
+        "Please list other collaborators that helped you in this project with links to there github profile! Also any third-party assets that require attribution. Finally any tutorials please include the link to them as well.", //put these in a list
+    },
+    {
+      type: "input",
+      name: "badges",
+      message: "Please present any badges as well.",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "Now list the features this project has!", //another list for this?
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "How can a user contribute to this project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "Finally, how can we test this application to make sure everything runs smoothly?",
+    },
   ])
   .then((data) => {
-console.log(data);
+    console.log(data);
+    const myREADME = createREADME(data);
+    fs.writeFileSync("./README.md", myREADME);
   });
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
+function createREADME({
+  projectTitle,
+  description,
+  tableOfContents,
+  installation,
+  usage,
+  credits,
+  badges,
+  features,
+  contribution,
+  tests,
+}) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+      <title>ReadME-Generator</title>
+    </head>
+    <body>
+      <header>${projectTitle}</header>
+      <section>${description}</section>
+      <section>${tableOfContents}</section>
+      <section>${installation}</section>
+      <section>${usage}</section>
+      <section>${credits}</section>
+      <section>${badges}</section>
+      <section>${features}</section>
+      <section>${contribution}</section>
+      <section>${tests}</section>
+  
+      <script src="./index.js"></script>
+    </body>
+  </html>`;
+}
 
 // // TODO: Create a function to initialize app
 // function init() {}
